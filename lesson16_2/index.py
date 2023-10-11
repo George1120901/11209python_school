5ad434eb7dda6ca26bca545b3c4bc28e02e5e271
George1120901 <PythonPro0901@gmail.com>
2023-10-11 15:36:34 +0800

    Case_update

---
+++ lesson16_2/index.py
@@ -0,0 +1,10 @@
+import dataSource
+
+def main():
+    cities = dataSource.cities_info()
+    cities1 = dataSource.cities_info()
+    for city in cities:
+        print(city)
+
+if __name__ == "__main__":
+    main()
