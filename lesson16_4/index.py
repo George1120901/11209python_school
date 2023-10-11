cb20bb72ddd6930754169282ff3a1bd02443d12b
George1120901 <PythonPro0901@gmail.com>
2023-10-11 16:51:17 +0800

    modify

--- lesson16_4/index.py
+++ lesson16_4/index.py
@@ -2,7 +2,7 @@ import dataSource
 
 def main():
     names = dataSource.cityNames()
-    city = dataSource.info(name='屏東縣新埤')
+    city = dataSource.info(name='屏東縣新埤鄉')
     #print(names)
     print(city)
     
