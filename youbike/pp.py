201721d66489683d4d425d9b655cfce36eac2a7d
George1120901 <PythonPro0901@gmail.com>
2023-10-26 14:41:49 +0800

    modify_youbike

--- youbike/pp.py
+++ youbike/pp.py
@@ -1,6 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import datasource
+
+class Window(tk.Tk):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
 
 class Window():
-    
+
 
 def main():
     pass
