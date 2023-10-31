97881148ef86d354f2a8b131da9724ff8b400870
George1120901 <pythonpro0901@gmail.com>
2023-10-31 23:29:51 +0800

    modify_lesson19HW

--- lesson19作業-231026/datasource.py
+++ lesson19作業-231026/datasource.py
@@ -8,7 +8,7 @@ import key
 
 #download data-----------------------------------------------------------------
 def __download_aqi_data()-> list:
-    aqi_url = f'https://data.moenv.gov.tw/api/v2/aqx_p_07?api_key={key.key}'
+    aqi_url = f'https://data.moenv.gov.tw/api/v2/aqx_p_07?formate=json&api_key={key.key}'
     response = requests.get(aqi_url)
     response.raise_for_status()
     print('下載成功')
@@ -17,7 +17,8 @@ def __download_aqi_data()-> list:
         json.dump(data['records'],file,ensure_ascii=False)
     f = open('data.json','r', encoding='utf-8')
     list_json=list(f)
-    print(f)
+    #list_json= json.loads(data)
+    print(list_json)
     return list_json
 
 
@@ -63,6 +64,16 @@ def update_sqlite_data()->None:
     conn = sqlite3.connect("空氣品質AQI.db")
     __create_table(conn)
     for item in data:
-        #print(item)
-        __insert_data(conn,values=[item[0],item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9]])
+        #__insert_data(conn,values=[item['siteid'],item['sitename'], item['siteengname'], item['areaname'], item['county'], item['township'], item['siteaddress'], item['twd97lon'], item['twd97lat'], item['sitetype']])
+
+        print(item)
+        
+        #__insert_data(conn, values=[f'{item[0]}', f'{item[1]}', f'{item[2]}', f'{item[3]}', f'{item[4]}', f'{item[5]}', f'{item[6]}', f'{item[7]}',  f'{item[8]}',  f'{item[9]}'])
+
+        #__insert_data(conn, values=[item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9] ])
+
+        #__insert_data(conn,values=[data["siteid"],data["sitename"], data["siteengname"], data["areaname"], data["county"], data["township"], data["siteaddress"], data["twd97lon"], data["twd97lat"], data["sitetype"]])
+
+        __insert_data(conn, values=[data[5], data[5], data[5], data[5], data[5], data[5], data[5], data[5], data[5], data[5]])
+
     conn.close()
