d2785dac31f0d91c569d15bb2aa7be6a54f2d761
George1120901 <PythonPro0901@gmail.com>
2023-11-01 14:01:00 +0800

    modify

--- lesson19作業-231026/datasource.py
+++ lesson19作業-231026/datasource.py
@@ -19,7 +19,7 @@ def download_aqi_data() -> list:
 
 # create sql able---------------------------------------
 def create_table(conn: sqlite3.Connection):
-    
+    print("判斷2 ok")
     sql= '''
 		CREATE TABLE IF NOT EXISTS "空氣品質監測站"(
             "id" INTEGER,
@@ -41,31 +41,36 @@ def create_table(conn: sqlite3.Connection):
     cursor=conn.cursor()
     cursor.execute(sql)    
     conn.commit()
-
+    print("判斷3 ok")
 
 def insert_data(conn: sqlite3.Connection, values: list):
+    
     cursor = conn.cursor()
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M')  
     sql = '''
 		REPLACE INTO '空氣品質監測站"("測站編號","測站名稱","測站英文名稱","空品區","城市","鄉鎮","測站地址","經度","緯度","測站類型","更新時間")
 		VALUES(?,?,?,?,?,?,?,?,?,?,?)
 	'''
     values.append(current_time)
     cursor.execute(sql, values)
+    cursor.close() 
+    conn.close()
     conn.commit()
+    
 
 
 def update_sqlite_data():
     data = download_aqi_data()
+    print("判斷4 ok")
     conn = sqlite3.connect("空氣品質監測站.db")
     create_table(conn)
+    print("判斷5 ok")
     for item in data:
         insert_data(conn,values=[item['siteid'],item['sitename'], item['siteengname'], item['areaname'], item['county'], item['township'], item['siteaddress'], item['twd97lon'], item['twd97lat'], item['sitetype']])
-
         print(item)
 
         print("更新完成")
-
     conn.close()
+    print("判斷6 ok")
 
 
