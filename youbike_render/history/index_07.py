ce8f3a95c93e699a3b53929175c64399ea619b0a
George1120901 <PythonPro0901@gmail.com>
2023-11-10 13:55:49 +0800

    modify_render

---
+++ youbike_render/history/index_07.py
@@ -0,0 +1,12 @@
+import datasource
+import psycopg2
+import password as pw
+
+def main():
+    datasource.updata_render_data()
+    datasource.updata_render_data()
+
+    
+
+if __name__ == "__main__":
+    main()
