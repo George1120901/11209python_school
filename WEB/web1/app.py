3cc9e4e450f59c8bdc66c5466a6f0e08701a99cc
George1120901 <PythonPro0901@gmail.com>
2023-11-29 03:26:29 +0000

    modify_web1

--- WEB/web1/app.py
+++ WEB/web1/app.py
@@ -1,12 +1,12 @@
 from flask import Flask,url_for,render_template
 import pandas as pd
 from auth import auth
-from web1.bs import bootstrap_bs04
+from bs import bootstrap
 
 
 app = Flask(__name__)
 app.register_blueprint(auth.bp)
-app.register_blueprint(bootstrap_bs04.bp)
+app.register_blueprint(bootstrap.bp)
 
 @app.route('/')
 def index():
