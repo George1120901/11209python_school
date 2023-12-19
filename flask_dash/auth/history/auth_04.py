7ea24f1a481da11969f202c92e3dcc02354e6a3e
George1120901 <PythonPro0901@gmail.com>
2023-12-19 04:35:18 +0000

    flask_20231219_1235

--- flask_dash/auth/history/auth_04.py
+++ flask_dash/auth/history/auth_04.py
@@ -5,16 +5,12 @@ from wtforms.validators import DataRequired
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
 
-class MyForm(FlaskForm):
-    name = StringField('姓名', validators=[DataRequired()])
-
 @blueprint_auth.route('/',methods=['GET', 'POST'])
 @blueprint_auth.route('/login',methods=['GET', 'POST'])
 def login():
-    form = MyForm()
     if request.method == "POST":
         print(request.form['email'])
         print(request.form['password'])
     
 
-    return render_template("/auth/login.html",form=form)
+    return render_template("/auth/login.html")
